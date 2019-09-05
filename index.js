ab4471067bb8a5d7748314ac3b1d1e5ad9abb517
kotamat <kota1861@gmail.com>
2019-09-05 22:51:48 +0900

    format

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
-const { composeWithJson } = require('graphql-compose-json');
-const { schemaComposer } = require('graphql-compose');
-const { printSchema } = require('graphql');
-const argv = require('yargs')
-.options({
-    input:{
-        alias: 'i',
-        describe:'target json file\'s path',
-        demandOption: true
+const { composeWithJson } = require("graphql-compose-json");
+const { schemaComposer } = require("graphql-compose");
+const { printSchema } = require("graphql");
+const argv = require("yargs")
+  .options({
+    input: {
+      alias: "i",
+      describe: "target json file's path",
+      demandOption: true
     },
     name: {
-        alias: 'n',
-        describe: 'name for query type',
-        demandOption: true
+      alias: "n",
+      describe: "name for query type",
+      demandOption: true
     },
     key: {
-        alias: 'k',
-        describe: 'key for query',
-        demandOption: true
+      alias: "k",
+      describe: "key for query",
+      demandOption: true
     }
-})
-.help()
-.argv
+  })
+  .help().argv;
 const json = require(argv.input);
 
 const GqlType = composeWithJson(argv.name, json);
 schemaComposer.Query.addFields({
   [argv.key]: {
     type: GqlType,
-    args: {
-    }
+    args: {}
   }
 });
 
-const schema = schemaComposer.buildSchema()
+const schema = schemaComposer.buildSchema();
 console.log(printSchema(schema));
